c872ba86b53e554724060d81fe8ed7653f84180e
lileee <112982735+lileeeeeee@users.noreply.github.com>
2022-11-04 17:34:51 -0500

    text appears in password box, nothing else works yet

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var userLength = window.prompt("Choose a password length between 8 and 128");
+var userSelection = window.prompt("please enter the values you would like to include below, separated by a comma");
+
+function generatePassword () {
+  if (userLength < 8 || userLength > 128) {
+    window.alert ("Please choose a password length from 8 to 128");
+    window.prompt("Choose a password length between 8 and 128");
+  }
+  else if (userSelection = "lowercase") {
+    console.log("this part works");
+}
+}
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+var password = generatePassword()
+
+  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,3 +28,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
