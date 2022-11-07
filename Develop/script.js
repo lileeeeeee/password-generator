17c32a9fb2c96fde8c0867086aa47cd54e76d7f5
lileee <112982735+lileeeeeee@users.noreply.github.com>
2022-11-07 13:10:09 -0600

    user inputs and checks done

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+//collects and checks user inputs for length//
 var userLength = window.prompt("Choose a password length between 8 and 128");
-var userSelection = window.prompt("please enter the values you would like to include below, separated by a comma");
-
-function generatePassword () {
-  if (userLength < 8 || userLength > 128) {
-    window.alert ("Please choose a password length from 8 to 128");
-    window.prompt("Choose a password length between 8 and 128");
+while (userLength < 8 || userLength > 128) {
+    var userLength = window.prompt ("Please choose a password length from 8 to 128");
+  
   }
-  else if (userSelection = "lowercase") {
-    console.log("this part works");
+var userSelection = window.prompt("Your password can contain uppercase letters, lowercase letters, numbers and or symbols. Please enter the values you would like to include below, separated by a comma. You must choose at least one.");
+
+//collects and checks user input for type of characters and requires at least one 
+
+if ((userSelection.includes("uppercase letters") || userSelection.includes("lowercase letters") || userSelection.includes("numbers") || userSelection.includes("symbols")  === true)) {
+window.alert("click Generate Password to see your password");
 }
+else if ((userSelection.includes("uppercase letters") && userSelection.includes("lowercase letters") && userSelection.includes("numbers") && userSelection.includes("symbols")  === true)) {
+var userSelection = window.prompt ("Your password must contain at least one of the following attributes: can contain uppercase letters, lowercase letters, numbers and or symbols. Please enter the values you would like to include below, separated by a comma.");
 }
 
+//will generate a password based on user inputs, only shows a random integer right now, is not affected by user input//
+function generatePassword () {
+  return Math.random();
+}
 
 // Write password to the #password input
 function writePassword() {
