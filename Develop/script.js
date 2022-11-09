fe8dd818a182c5191a6cf948543d3a5d523710ef
lileee <112982735+lileeeeeee@users.noreply.github.com>
2022-11-09 17:25:12 -0600

    removed unnecessary code and updated README

--- Develop/script.js
+++ Develop/script.js
@@ -13,7 +13,7 @@ while (userLength < 8 || userLength > 128 || isNaN(userLength)) {
   }
   
 // collects user selections for password characters in a single string
-var userSelection = window.prompt("Your password can contain uppercase letters, lowercase letters, numbers and or symbols. Please enter the values you would like to include below, separated by a comma. You must choose at least one.");
+var userSelection = window.prompt("Your password can contain uppercase letters, lowercase letters, numbers and/or symbols. Please enter the values you would like to include below, separated by a comma. You must choose at least one.");
 
 //collects and checks user input for type of characters and requires at least one 
 
@@ -21,66 +21,17 @@ if ((userSelection.includes("uppercase letters") || userSelection.includes("lowe
 window.alert("click Generate Password to see your password");
 }
 while ((userSelection.includes("uppercase letters") === false && userSelection.includes("lowercase letters") === false && userSelection.includes("numbers") === false && userSelection.includes("symbols")  === false)) {
-var userSelection = window.prompt ("Your password must contain at least one of the following attributes: can contain uppercase letters, lowercase letters, numbers and or symbols. Please enter the values you would like to include below, separated by a comma.");
+var userSelection = window.prompt ("Your password must contain at least one of the following attributes: uppercase letters, lowercase letters, numbers and or symbols. Please enter the values you would like to include below, separated by a comma.");
 }
 //checks user selections in console
 console.log(userSelection);
 console.log(userLength);
-
-
 
 // password pieces
 var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
 var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowercaseLetters = ["a","b","c","d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// possibleCharacters = a concatenation of the paramaters
-
-//functions do not need these
-function rdmNum  () {
- if (userSelection.includes("numbers")) 
-return numbers[Math.floor(Math.random()*numbers.length)] 
-}
-
-function rdmSymbol () {
-   if (userSelection.includes("symbols"))
-  return symbol[Math.floor(Math.random()*symbol.length)];
-  }
-
-function rdmUppercase () {
-  if (userSelection.includes("uppercase letters"))
-  return (uppercaseLetters)[Math.floor(Math.random()*uppercaseLetters.length)];
-}
-
-function rdmLowercase () {
-  if (userSelection.includes("lowercase letters"))
-  return (lowercaseLetters)[Math.floor(Math.random()*lowercaseLetters.length)];
-}
-
-//need this code, commented out to test a more efficient method
-
-//var passwordString = ""
-//while (passwordString.length < userLength) {
-//if (userSelection.includes("numbers") === true && passwordString.length < userLength) {
- // passwordString = passwordString + numbers[Math.floor(Math.random()*numbers.length)]
-//}
-//console.log(passwordString);
-//if (userSelection.includes("symbols") === true && passwordString.length < userLength) {
-//passwordString = passwordString + symbol[Math.floor(Math.random()*symbol.length)]
-//}
-//console.log(passwordString);
-//if (userSelection.includes("uppercase letters") && passwordString.length < userLength) {
-//  passwordString = passwordString + uppercaseLetters[Math.floor(Math.random()*uppercaseLetters.length)]
-//}
-//console.log(passwordString);
-//if (userSelection.includes("lowercase letters")&& passwordString.length < userLength) {
-//  passwordString = passwordString + (lowercaseLetters)[Math.floor(Math.random()*lowercaseLetters.length)];
-//}
-//console.log(passwordString.length);
-//}
-
-// var possibleCharacters = [];
 
 // builds an array based on user parameters
 var possibleCharacters = [];
@@ -101,28 +52,11 @@ if (userSelection.includes("lowercase letters")) {
 }
 console.log(possibleCharacters);
 
-// for (let i = 0; i < userLength; i++) {
-//  passwordString = passwordString + possibleCharacters[Math.floor(Math.random()*possibleCharacters.length)]
-// }
+// iterates through the array of user selected options until the user's chosen length is met //
 var passwordString = " ";
 for (let i = 0; i < userLength; i++) {
    passwordString = passwordString + ((possibleCharacters[Math.floor(Math.random()*possibleCharacters.length)]));
- 
 }
-//console.log(passwordString);
-
-console.log(possibleCharacters[Math.floor(Math.random()*possibleCharacters.length)]);
-
-console.log(Math.floor(Math.random()*possibleCharacters.length));
-
-
-//while (passwordString < userLength) {
-//  var passwordString = passwordString + rdmNum  () + rdmSymbol () + rdmUppercase () + rdmLowercase () + passwordString;
-//}
-
-
-
-//Generates password based on user inputs using the above functions//
 
   return passwordString;
 }
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # password-generator
 An app for generating random passwords
+
+## Description
+This is a website that will generate a random password. The password length and characters used are user selected. 
+
+## Installation
+Website is available here: https://lileeeeeee.github.io/password-generator
+
+## Usage
+When the "Generate Password" button is clicked, the user will be prompted to enter a password length between 8 and 128 and select which character types they would like to include. If the user's answers do not fit within the parameters offered, the popup will appear again until the conditions are met. 
+
+Credits
+N/A
+
+License
+MIT License
